4e583a639517cc24c547c11ac4449396cb5781c4
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 21:28:03 -0400

    Add files via upload

---
+++ DropDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDown : MonoBehaviour
+{
+    [HideInInspector]
+    public int arrayIdx = 0;
+    [HideInInspector]
+    public string[] MyArray = new string[] { "Linsay", "Betty", "Wilma", "Eden" };
+
+    [HideInInspector]
+    public int listIdx = 0;
+    [HideInInspector]
+    public List<string> MyList = new List<string>(new string[] { "Omar", "Henry", "Leslie", "Chris" });
+
+}
---
+++ DropdownEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DropDown))]
+public class DropdownEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        DropDown script = (DropDown)target;
+
+        GUIContent arrayLabel = new GUIContent("MyArray");
+        script.arrayIdx = EditorGUILayout.Popup(arrayLabel, script.arrayIdx, script.MyArray);
+
+        GUIContent arrayList = new GUIContent("MyList");
+        script.listIdx = EditorGUILayout.Popup(arrayList, script.listIdx, script.MyList.ToArray());
+    }
+}
